7779f9ab9f2cd1e19f2c0c811f8ede90f181d4f7
weiqif <weiqif@andrew.cmu.edu>
2018-05-05 08:59:03 -0400

    all of it

--- relationship.js
+++ relationship.js
@@ -2,11 +2,11 @@
 function find_related(idx, table) //idx is the index of professor 1
 {
 	var relation_array = [];
-	for ( var i = 0; i < 115; i ++ ) 
+	for ( var i = 0; i < 50; i ++ ) 
 	{
 		if (i != idx) 
 		{
-			if (table[i].R1 == table[idx].R1 || table[i].R1 == table[idx].R2 || table[i].R1 == table[idx].R3 || table[i].R1 == table[idx].R4 || table[i].R1 == table[idx].R5)
+			if (table[i].AcePer == table[idx].R1 || table[i].R1 == table[idx].R2 || table[i].R1 == table[idx].R3 || table[i].R1 == table[idx].R4 || table[i].R1 == table[idx].R5)
 			{
 				relation_array.push(i);
 				relation_array.push(table[i].R1);
@@ -46,19 +46,5 @@ function find_related(idx, table) //idx is the index of professor 1
 	relation_array.push(idx);
 	relation_array.push(table[idx].R1);
 	return relation_array;
-}
-
-function department_range(idx) //idx of professor
-{
-	if(idx >= 0 && idx <= 28)
-		return [0, 28];
-	if(idx >28 && idx <= 53)
-		return [29, 53];
-	if(idx > 53 && idx <= 95)
-		return [54, 95];
-	if(idx > 95 && idx <= 118)
-		return [96, 118];
-	else
-		return -1;
 }
 
